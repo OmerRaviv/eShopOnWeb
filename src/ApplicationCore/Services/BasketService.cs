9f1e04ff0a02c5f6f8bff23b4d14e9431fbd0507
Alon <alonf@codevalue.net>
2019-09-20 22:53:30 +0300

    Add a bug

--- src/ApplicationCore/Services/BasketService.cs
+++ src/ApplicationCore/Services/BasketService.cs
@@ -59,7 +59,8 @@ namespace Microsoft.eShopWeb.ApplicationCore.Services
             {
                 if (quantities.TryGetValue(item.Id.ToString(), out var quantity))
                 {
-                    if(_logger != null) _logger.LogInformation($"Updating quantity of item ID:{item.Id} to {quantity}.");
+                    Guard.Against.OutOfRange(quantity, nameof(quantity), 0, int.MaxValue);
+                    if (_logger != null) _logger.LogInformation($"Updating quantity of item ID:{item.Id} to {quantity}.");
                     item.Quantity = quantity;
                 }
             }
