077afbe0b7510458fa2fc5605a625c18603e4703
Idan Shatz <idans@oz-code.com>
2020-06-25 10:33:37 -0600

    adding settings page

---
+++ src/Web/Pages/Settings.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Microsoft.eShopWeb.Web.Pages
+{
+    public class SettingsModel : PageModel
+    {
+        public bool AgentSettingFound { get; private set; } = false;
+
+        private IHostingEnvironment _hostingEnvironment;
+
+        public SettingsModel(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public void OnGet()
+        {
+            if (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("OzCode_Agent_Token"))) {
+                AgentSettingFound = true;
+            }
+        }
+
+        public bool IsProduction
+        {
+            get
+            {
+                return _hostingEnvironment.IsProduction();
+            }
+        }
+
+        public bool IsStaging
+        {
+            get
+            {
+                return _hostingEnvironment.IsStaging();
+            }
+        }
+
+        public bool IsDevelopment
+        {
+            get
+            {
+                return _hostingEnvironment.IsDevelopment();
+            }
+        }
+    }
+}
